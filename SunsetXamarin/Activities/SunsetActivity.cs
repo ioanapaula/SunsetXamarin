302043661453e006c11603233bd4b96be272a16a
Ioana-Paula Stroe <stroe.ioanapaula@gmail.com>
2019-05-03 11:27:26 +0300

    Chapter-32: Property Animation - Theory

---
+++ SunsetXamarin/Activities/SunsetActivity.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using SunsetXamarin.Fragments;
+
+namespace SunsetXamarin
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class SunsetActivity : SingleFragmentActivity
+    {
+        protected override Android.Support.V4.App.Fragment CreateFragment()
+        {
+            return SunsetFragment.NewInstance();
+        }
+    }
+}
